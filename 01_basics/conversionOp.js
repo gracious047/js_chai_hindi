302b0f69612ad7be460f8df138383e6ec86d6c27
gracious047 <pratikkumar0793@gmail.com>
2025-08-14 06:00:46 +0000

    ConversionOperations

---
+++ 01_basics/conversionOp.js
@@ -0,0 +1,32 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "pratik"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//*Note
+// 33 -> 33
+// "33abc", undefined -> NaN
+// true -> 1; false -> 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+let isLoggedIn = "Pratik"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//*Note
+// 1 -> true; 0 -> false
+// "" -> false
+// "Pratik" -> true
