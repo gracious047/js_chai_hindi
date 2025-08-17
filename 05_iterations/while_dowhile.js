2a9de1aeb390c376442454a92fd7103775726148
gracious047 <pratikkumar0793@gmail.com>
2025-08-17 12:23:20 +0000

    whileloops

---
+++ 05_iterations/while_dowhile.js
@@ -0,0 +1,22 @@
+// While loops
+let index = 0
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+let myArray = ["Flash", "Batman", "Supreman"]
+let arr = 0
+while(arr < myArray.length){
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+// do while loop
+
+// let score = 1
+let score = 11
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while (score <= 10)
